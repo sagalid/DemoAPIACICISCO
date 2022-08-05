bfbdda8b71a70b928deba40138a97cda3fcfae1f
Agustin <agustin.salas@sagalid.cl>
2022-08-04 20:10:57 -0400

    Agrego nueva función para obtener el token del API.

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import json
+import requests
 import conf
-print(conf.clave)
-print(conf.usuario)
+
+sandbox = "https://sandboxapicdc.cisco.com"
+
+
+def obtener_token(usuario, clave):
+    url = sandbox + "/api/aaaLogin.json"
+    body = {
+        "aaaUser": {
+            "attributes": {
+                "name": usuario,
+                "pwd": clave
+            }
+        }
+    }
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    token = respuesta.json()['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+
+token = obtener_token(conf.usuario, conf.clave)
+
+print(token)
